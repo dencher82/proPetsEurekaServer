a0c530e8a2ff1f7050f5eda8a517b00e443761d9
dencher82 <den_zh@live.com>
2021-01-23 20:07:17 +0200

    first commit

---
+++ src/test/java/propets/eureka/ProPetsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package propets.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
